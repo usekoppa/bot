b543a17375dddcdc54e1580c60d1bb0f338e8cd4
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-05-17 17:36:24 +1000

    chore(src): remove pointless files

--- src/login.ts
+++
@@ -1,8 +0,0 @@
-import { KoppaClient } from "@core/client";
-import { config } from "@utils/config";
-
-import { Container } from "typedi";
-
-const client = Container.get(KoppaClient);
-
-void client.login(config.bot.token);
