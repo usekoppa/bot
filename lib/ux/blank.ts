32ff118e290cb439daef26a47a4efce0c829ed33
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-05-29 16:26:21 +1000

    docs(ux/blank): document about line break

--- lib/ux/blank.ts
+++ lib/ux/blank.ts
@@ -1,2 +1,3 @@
-// This is the blank symbol in braille (6 empty spots). It can be used as a break in Discord embeds.
+// This is the blank symbol in braille (6 empty spots).
+// It can be used as a break in Discord embeds and works on both mobile and desktop.
 export const blank = "\u2800";
