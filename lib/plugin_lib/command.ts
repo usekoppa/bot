de7fea872c08889d683add76372467df9dd34d0f
zorbyte <39759284+zorbyte@users.noreply.github.com>
2021-08-21 18:53:55 +1000

    feat(cmds): more command functionality

--- lib/plugin_lib/command.ts
+++ lib/plugin_lib/command.ts
@@ -1,5 +1,8 @@
+import { Asyncable } from "@utils/types";
+
 import {
   ApplicationCommandData,
+  ApplicationCommandOptionChoice,
   ApplicationCommandOptionData,
   Channel,
   Role,
@@ -22,6 +25,18 @@ interface OptionTypesMap {
 }
 
 abstract class BaseBuilder {
+  protected _name?: string;
+  protected _description?: string;
+  get name() {
+    this.throwIfNotSet(this._name);
+    return this._name!;
+  }
+
+  get description() {
+    this.throwIfNotSet(this._description);
+    return this._description!;
+  }
+
   protected throwIfNotSet(data: unknown) {
     if (
       typeof data === "undefined" ||
@@ -41,43 +56,68 @@ class Option<
   extends BaseBuilder
   implements ApplicationCommandOptionData
 {
-  #name!: string;
-  #required = false;
-  #description!: string;
+  _required = false;
 
   constructor(public type: T) {
     super();
   }
 
-  get name() {
-    this.throwIfNotSet(this.#name);
-    return this.#name;
-  }
-
-  get description() {
-    return this.#description;
-  }
-
   get required() {
-    this.throwIfNotSet(this.#required);
-    return this.#required as R;
+    this.throwIfNotSet(this._required);
+    return this._required as R;
   }
 
   setName<NN extends string>(name: NN) {
-    this.#name = name;
+    this._name = name;
     return this as unknown as Option<T, NN, R>;
   }
 
   setDescription(description: string) {
-    this.#description = description;
+    this._description = description;
     return this;
   }
 
-  isRequired<IR extends boolean>(required: IR) {
-    this.#required = required;
+  setRequired<IR extends boolean>(required: IR) {
+    this._required = required;
     return this as unknown as Option<T, N, IR>;
   }
 }
+
+class Context<A = Record<string, never>> {
+  args = {} as A;
+}
+
+class OptionWithChoices<
+  T extends
+    | ApplicationCommandOptionTypes.INTEGER
+    | ApplicationCommandOptionTypes.STRING,
+  N extends string = string,
+  R extends boolean = false,
+  C extends OptionTypesMap[T] = never
+> extends Option<T, N, R> {
+  choices?: ApplicationCommandOptionChoice[];
+
+  setName<NN extends string>(name: NN) {
+    this._name = name;
+    return this as unknown as OptionWithChoices<T, NN, R, C>;
+  }
+
+  setRequired<IR extends boolean>(required: IR) {
+    this._required = required;
+    return this as unknown as OptionWithChoices<T, N, IR, C>;
+  }
+
+  addChoice<V extends OptionTypesMap[T]>(name: string, value: V) {
+    if (typeof this.choices === "undefined") this.choices = [];
+    this.choices.push({ name, value });
+
+    return this as unknown as OptionWithChoices<T, N, R, C | V>;
+  }
+}
+
+type Middleware<A = Record<string, never>> = (
+  ctx: Context<A>
+) => Asyncable<void>;
 
 type ArgumentContainer<
   T extends ApplicationCommandOptionTypes,
@@ -91,21 +131,58 @@ type AddOptionCb<
   R extends boolean
 > = (opt: Option<T>) => Option<T, N, R>;
 
+type AddOptionWithChoiceCb<
+  T extends
+    | ApplicationCommandOptionTypes.INTEGER
+    | ApplicationCommandOptionTypes.STRING,
+  N extends string,
+  R extends boolean,
+  C extends OptionTypesMap[T]
+> = (opt: OptionWithChoices<T>) => OptionWithChoices<T, N, R, C>;
+
 // eslint-disable-next-line @typescript-eslint/ban-types
-class Command<A = {}> extends BaseBuilder implements ApplicationCommandData {
-  #name?: string;
-  #description?: string;
+type Subcommand<A = {}> = Omit<
+  Command<A, true>,
+  "addSubcommand" | "addSubcommandGroup"
+>;
+
+class SubcommandGroup extends BaseBuilder {
+  readonly type = ApplicationCommandOptionTypes.SUB_COMMAND_GROUP;
+  options: Subcommand[] = [];
+
+  setName(name: string) {
+    this._name = name;
+    return this;
+  }
+
+  setDescription(description: string) {
+    this._description = description;
+    return this;
+  }
+
+  addSubcommand(fn: (subcommand: Subcommand) => Subcommand) {
+    const cmd = new Command(true);
+    const subCmd = fn(cmd);
+    this.options.push(subCmd);
+
+    return this as Omit<this, "run">;
+  }
+}
+
+// eslint-disable-next-line @typescript-eslint/ban-types
+export class Command<A = {}, S extends boolean = false>
+  extends BaseBuilder
+  implements ApplicationCommandData
+{
+  type: ApplicationCommandOptionTypes = undefined!;
+  #stack: Middleware<A>[] = [];
   #options: ApplicationCommandOptionData[] = [];
   #defaultPermissionEnabled = true;
 
-  get name() {
-    this.throwIfNotSet(this.#name);
-    return this.#name!;
-  }
+  constructor(public isSubcommand: S = false as S) {
+    super();
 
-  get description() {
-    this.throwIfNotSet(this.#description);
-    return this.#description!;
+    if (isSubcommand) this.type = ApplicationCommandOptionTypes.SUB_COMMAND;
   }
 
   get defaultPermission() {
@@ -117,12 +194,12 @@ class Command<A = {}> extends BaseBuilder implements ApplicationCommandData {
   }
 
   setName(name: string) {
-    this.#name = name;
+    this._name = name;
     return this;
   }
 
   setDescription(description: string) {
-    this.#description = description;
+    this._description = description;
     return this;
   }
 
@@ -131,15 +208,63 @@ class Command<A = {}> extends BaseBuilder implements ApplicationCommandData {
     return this;
   }
 
-  addStringOption<N extends string, R extends boolean>(
-    cb: AddOptionCb<ApplicationCommandOptionTypes.STRING, N, R>
-  ) {
-    return this.#addOption(ApplicationCommandOptionTypes.STRING, cb);
+  addSubcommand(fn: (subcommand: Subcommand) => Subcommand) {
+    const cmd = new Command(true);
+    const subCmd = fn(cmd);
+    this.#options.push(subCmd);
+    return this as Omit<this, "run">;
   }
 
-  // TODO(@zorbyte): Save the function and have a means to build the obj.
-  run(cb: (ctx: { args: A }) => void) {
-    cb({ args: {} as A });
+  addSubcommandGroup(fn: (group: SubcommandGroup) => SubcommandGroup) {
+    const group = new SubcommandGroup();
+    const finalGroup = fn(group);
+    this.#options.push(finalGroup);
+    return this as Omit<this, "run">;
+  }
+
+  addStringOption<
+    T extends ApplicationCommandOptionTypes.STRING,
+    N extends string,
+    R extends boolean,
+    C extends string
+  >(fn: AddOptionWithChoiceCb<T, N, R, C>) {
+    return this.#addOptionWithChoices<T, N, R, C>(
+      ApplicationCommandOptionTypes.STRING as T,
+      fn
+    );
+  }
+
+  addNumberOption<N extends string, R extends boolean>(
+    fn: AddOptionCb<ApplicationCommandOptionTypes.NUMBER, N, R>
+  ) {
+    return this.#addOption(ApplicationCommandOptionTypes.NUMBER, fn);
+  }
+
+  use(fn: Middleware<A> | Middleware<A>[]) {
+    this.#stack.push(...(Array.isArray(fn) ? fn : [fn]));
+    return this;
+  }
+
+  run(fn: Middleware<A>) {
+    this.#stack.push(fn);
+    return this;
+  }
+
+  #addOptionWithChoices<
+    T extends
+      | ApplicationCommandOptionTypes.INTEGER
+      | ApplicationCommandOptionTypes.STRING,
+    N extends string,
+    R extends boolean,
+    C extends OptionTypesMap[T]
+  >(type: T, cb: AddOptionWithChoiceCb<T, N, R, C>) {
+    const opt = new OptionWithChoices(type);
+    const newOpt = cb(opt);
+    this.#options.push(newOpt);
+    return this as unknown as Command<
+      A & Record<N, C | (R extends false ? undefined : never)>,
+      S
+    >;
   }
 
   #addOption<
@@ -150,6 +275,6 @@ class Command<A = {}> extends BaseBuilder implements ApplicationCommandData {
     const opt = new Option(type);
     const newOpt = cb(opt);
     this.#options.push(newOpt);
-    return this as unknown as Command<A & ArgumentContainer<T, N, R>>;
+    return this as unknown as Command<A & ArgumentContainer<T, N, R>, S>;
   }
 }
